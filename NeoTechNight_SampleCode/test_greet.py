61a6e7a9c12f15a2836b5f4f79b37b71e27579b0
miya <baozi1404954311@gmail.com>
2025-04-16 17:52:48 +0900

    add: dockerでwordpressを立ち上げるymlを追加

--- NeoTechNight_SampleCode/test_greet.py
+++ NeoTechNight_SampleCode/test_greet.py
@@ -5,8 +5,5 @@ class TestGreet(unittest.TestCase):
     def test_greet_returns_hello_taro(self):
         self.assertEqual(greet("Taro"), "Hello, Taro")
 
-    def test_greet_returns_hello_hanako(self):
-        self.assertEqual(greet("Hanako"), "Hello, Hanako")
-
 if __name__ == '__main__':
     unittest.main()
